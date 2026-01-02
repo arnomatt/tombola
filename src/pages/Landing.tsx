a848c233e86893a5b8e6ab2f5dcad8849204c816
arnomatt <matteo.arno@gmail.com>
2026-01-02 16:53:45 +0000

    styling landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,15 +1,14 @@
 import { useNavigate } from "react-router";
+import './Landing.css';
 
 export const Landing = () => {
     const navigate = useNavigate();
 
     return (
-        <>
+        <div className='landing'>
             <div>Landing</div>
-            <div>
-                <button onClick={() => navigate('/cartelle')}>Cartelle</button>
-                <button onClick={() => navigate('/tabellone')}>Tabellone</button>
-            </div>
-        </>
+            <button onClick={() => navigate('/cartelle')}>Cartelle</button>
+            <button onClick={() => navigate('/tabellone')}>Tabellone</button>
+        </div>
     )
 }
