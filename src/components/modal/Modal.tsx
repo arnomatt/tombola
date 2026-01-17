48a5589dba11b4ec7e8f1bcf45a06fe542b449d2
arnomatt <matteo.arno@gmail.com>
2026-01-17 21:04:29 +0000

    style changes

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -19,7 +19,7 @@ export const Modal = () => {
     return (
         <div className='modal'>
             <button onClick={() => context?.setModalVisibility(false)} className='closeButton'>X</button>
-            <button onClick={() => handleClick()}>1 Cartella</button>
+            <button onClick={() => handleClick(1)}>1 Cartella</button>
             <button onClick={() => handleClick(2)}>2 Cartelle</button>
             <button onClick={() => handleClick(3)}>3 Cartelle</button>
             <button onClick={() => handleClick(4)}>4 Cartelle</button>
