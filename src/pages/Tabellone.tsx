dac5b393c0839657679ad01502265a4a7401c044
arnomatt <matteo.arno@gmail.com>
2026-02-05 21:14:11 +0000

    adding useTabellone hook

--- src/pages/Tabellone.tsx
+++ src/pages/Tabellone.tsx
@@ -1,11 +1,39 @@
 import './Tabellone.css';
 import '../components/cartelle/Cartella.css';
+import { useState } from 'react';
+
+const useTabellone = (): [Map<number, boolean>, (number: number) => void] => {
+    const tabelloneMap = new Map<number, boolean>();
+    Array.from({ length: 90 }).map((_, i) => tabelloneMap.set(i + 1, false));
+
+    const [tabellone, setTabellone] = useState(tabelloneMap);
+
+    const handleClick = (num: number) => {
+        setTabellone(prev => {
+            const newTabellone = new Map(prev);
+            newTabellone.set(num, !newTabellone.get(num))
+            return newTabellone;
+        })
+    }
+
+
+    return [tabellone, handleClick];
+}
 
 export const Tabellone = () => {
+    const [tabellone, handleClick] = useTabellone();
+
     return (
         <div className='container'>
             <div className='tabellone'>
-                {Array.from({ length: 90 }).map((_, i) => <div className='casella'>{i + 1}</div>)}
+                {Array.from(tabellone.entries()).map(([key, selected]) =>
+                    <button
+                        className={selected ? 'casella selezionata' : 'casella'}
+                        onClick={() => handleClick(key)}
+                    >
+                        {key}
+                    </button>
+                )}
             </div>
         </div >
     )
