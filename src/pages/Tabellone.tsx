a5f2b572eb444fe88a4f781ff27ecd095dab1b50
arnomatt <matteo.arno@gmail.com>
2026-02-06 11:30:53 +0000

    automate checking tabellone numbers on pick

--- src/pages/Tabellone.tsx
+++ src/pages/Tabellone.tsx
@@ -2,54 +2,50 @@ import './Tabellone.css';
 import '../components/cartelle/Cartella.css';
 import { useState } from 'react';
 
-const useTabellone = (): [Map<number, boolean>, (number: number) => void] => {
+const useTabellone = (): [Map<number, boolean>, number, () => void] => {
     const tabelloneMap = new Map<number, boolean>();
     Array.from({ length: 90 }).map((_, i) => tabelloneMap.set(i + 1, false));
+    const [pick, setPick] = useState(0);
 
     const [tabellone, setTabellone] = useState(tabelloneMap);
 
-    const handleClick = (num: number) => {
-        setTabellone(prev => {
-            const newTabellone = new Map(prev);
-            newTabellone.set(num, !newTabellone.get(num))
-            return newTabellone;
-        })
+    const handlePick = () => {
+        const itemsLeft = Array.from(tabellone.entries()).filter(item => !item[1]);
+        if (itemsLeft.length !== 0) {
+            const index = Math.floor((Math.random() * itemsLeft.length));
+            const num = itemsLeft[index][0];
+
+            console.log(itemsLeft);
+
+            setTabellone(prev => {
+                const newTabellone = new Map(prev);
+                newTabellone.set(num, !newTabellone.get(num))
+                return newTabellone;
+            })
+            setPick(num);
+        }
     }
 
-
-    return [tabellone, handleClick];
-}
-
-const usePesca = (): [number, () => void] => {
-    const [pescato, setPescato] = useState(1);
-
-    const handlePescaClick = () => {
-        const num = Math.floor((Math.random() * 90 + 1));
-        setPescato(num);
-    }
-
-    return [pescato, handlePescaClick]
+    return [tabellone, pick, handlePick];
 }
 
 export const Tabellone = () => {
-    const [tabellone, handleClick] = useTabellone();
-    const [pescato, handlePescaClick] = usePesca();
+    const [tabellone, pick, handlePick] = useTabellone();
 
     return (
         <div className='container'>
             <div className='tabellone'>
                 {Array.from(tabellone.entries()).map(([key, selected]) =>
-                    <button
+                    <div
                         className={selected ? 'casella selezionata' : 'casella'}
-                        onClick={() => handleClick(key)}
                     >
                         {key}
-                    </button>
+                    </div>
                 )}
             </div>
             <div className='pescaContainer'>
-                <button className='pesca' onClick={handlePescaClick}>
-                    <p className='pescato'>{pescato}</p>
+                <button className='pesca' onClick={handlePick}>
+                    <p className={pick !== 0 ? 'pescato' : undefined}>{pick === 0 ? 'Clicca per pescare un numero' : pick}</p>
                 </button>
             </div>
         </div >
