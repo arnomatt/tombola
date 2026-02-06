7c602ae8fffeaea67415265482069b7727a676ff
arnomatt <matteo.arno@gmail.com>
2026-02-06 11:39:49 +0000

    adding confirmation before navigating to home

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,12 +1,29 @@
-import { NavLink } from "react-router";
+import { NavLink, useLocation } from "react-router";
 import './NavBar.css';
 
 export const NavBar = () => {
+    const location = useLocation();
+
+    const handleHomeClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        if (location.pathname.includes('cartelle') || location.pathname.includes('tabellone')) {
+            const confirmed = window.confirm('Torna alla Home? I progressi di gioco saranno persi');
+            if (!confirmed) {
+                event.preventDefault();
+            }
+        }
+    };
+
     return (
         <div className="navBar">
-            <NavLink className={({ isActive }) =>
-                isActive ? "active" : "inactive"
-            } to='/'>Home</NavLink>
+            <NavLink
+                className={({ isActive }) =>
+                    isActive ? "active" : "inactive"
+                }
+                to='/'
+                onClick={handleHomeClick}
+            >
+                Home
+            </NavLink>
         </div>
-    )
+    );
 }
