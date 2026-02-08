1d0db22dc6a4415f14f7436e7f5684cb0834802f
arnomatt <matteo.arno@gmail.com>
2026-02-08 14:39:54 +0000

    cleaning up Cartelle

--- src/pages/Cartelle.tsx
+++ src/pages/Cartelle.tsx
@@ -2,22 +2,12 @@ import { useParams } from "react-router";
 import { Cartella } from "../components/cartelle/Cartella"
 import { buildCartella } from "../components/utils/generaCartella";
 import './Cartelle.css';
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 
 export const Cartelle = () => {
-    const initialScore = {
-        'Ambo': false,
-        'Terno': false,
-        'Quaterna': false,
-        'Cinquina': false,
-        'Tombola': false,
-    }
-
     const { num } = useParams();
-    const [score, setScore] = useState(initialScore);
 
     const buildCartelle = useMemo(() => {
-        console.log('Building Cartelle...');
         return (num ? Array.from({ length: parseInt(num) }).map((_, i) => (
             <Cartella key={i} numbers={buildCartella()} id={i + 1}></Cartella>
         )) : <Cartella numbers={buildCartella()} id={1}></Cartella>)
