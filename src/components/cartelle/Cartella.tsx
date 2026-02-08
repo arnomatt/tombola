1ee1f32e43d37c311a3283be751b5859e4cc5896
arnomatt <matteo.arno@gmail.com>
2026-02-08 14:53:51 +0000

    Revert "adding score animation"
    This reverts commit 79874b87a4c147c2ddcad7a84727b750b7efcee2.

--- src/components/cartelle/Cartella.tsx
+++ src/components/cartelle/Cartella.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import './Cartella.css';
-import { Score } from './Score';
 
 interface CartellaProps {
     id: number;
@@ -56,10 +55,7 @@ export const Cartella = (props: CartellaProps) => {
 
     return (
         <div>
-            <div className="title">
-                <div>Cartella #{id}</div>
-                <Score score={scores.get(score) || '-'}></Score>
-            </div>
+            <div className="title">Cartella #{id} - Score: {scores.get(score) || '-'}</div>
             <div className="cartella">
                 {numbers.map((row, idr) => row.map((number, idx) =>
                     number ?
