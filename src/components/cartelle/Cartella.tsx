668602e29c6baff60b9db054705664eb65356233
arnomatt <matteo.arno@gmail.com>
2026-02-08 14:05:25 +0000

    mapping scores to text

--- src/components/cartelle/Cartella.tsx
+++ src/components/cartelle/Cartella.tsx
@@ -6,6 +6,8 @@ interface CartellaProps {
     numbers: number[][]; // 3 rows x 9 columns, 0 means empty cell
 }
 
+const scores = new Map([[2, 'Ambo'], [3, 'Terno'], [4, 'Quaterna'], [5, 'Cinquina'], [15, 'Tombola']]);
+
 /**
  * A custom hook to handle the logic of each card
  * In a card, each row will have 5 numbers and each column will have at least 1 number
@@ -30,11 +32,16 @@ const useCartella = (numbers: number[]): [number, Map<string, boolean>, (number:
             const newCartella = new Map(prevCartella);
             newCartella.set(key, !newCartella.get(key));
             const array = Array.from(newCartella.entries());
-            const row1 = array.slice(0, 5);
-            const row2 = array.slice(5, 10);
-            const row3 = array.slice(10, 15);
-            const currentScore = Math.max(row1.filter(i => i[1]).length, row2.filter(i => i[1]).length, row3.filter(i => i[1]).length);
-            setScore(currentScore);
+            const row1Sum = array.slice(0, 5).filter(i => i[1]).length;
+            const row2Sum = array.slice(5, 10).filter(i => i[1]).length;
+            const row3Sum = array.slice(10, 15).filter(i => i[1]).length;
+            const totalSum = row1Sum + row2Sum + row3Sum;
+            if (totalSum === 15) {
+                setScore(15);
+            } else {
+                const currentScore = Math.max(row1Sum, row2Sum, row3Sum);
+                setScore(currentScore);
+            }
             return newCartella;
         })
     }
@@ -48,7 +55,7 @@ export const Cartella = (props: CartellaProps) => {
 
     return (
         <div>
-            <div className="title">Cartella no. {id} - Score: {score}</div>
+            <div className="title">Cartella #{id} - Score: {scores.get(score) || '-'}</div>
             <div className="cartella">
                 {numbers.map((row, idr) => row.map((number, idx) =>
                     number ?
