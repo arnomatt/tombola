02a35b4e8f1ac3dd77fd8a11aac36fbfae3e643e
arnomatt <matteo.arno@gmail.com>
2026-02-08 13:43:39 +0000

    adding scores to cartelle

--- src/components/cartelle/Cartella.tsx
+++ src/components/cartelle/Cartella.tsx
@@ -13,7 +13,7 @@ interface CartellaProps {
  * @param numbers 
  * @returns [cartella, handleNumberClick] which are the state and the click handler to update it
  */
-const useCartella = (numbers: number[]): [Map<string, boolean>, (number: number) => void] => {
+const useCartella = (numbers: number[]): [number, Map<string, boolean>, (number: number) => void] => {
     const validNumbers = numbers.filter(number => number !== 0);
     const cartellaMap = new Map<string, boolean>();
 
@@ -22,26 +22,33 @@ const useCartella = (numbers: number[]): [Map<string, boolean>, (number: number)
     }
 
     const [cartella, setCartella] = useState(cartellaMap);
+    const [score, setScore] = useState(0);
 
     const handleNumberClick = (number: number) => {
         const key = number.toString();
         setCartella(prevCartella => {
             const newCartella = new Map(prevCartella);
             newCartella.set(key, !newCartella.get(key));
+            const array = Array.from(newCartella.entries());
+            const row1 = array.slice(0, 5);
+            const row2 = array.slice(5, 10);
+            const row3 = array.slice(10, 15);
+            const currentScore = Math.max(row1.filter(i => i[1]).length, row2.filter(i => i[1]).length, row3.filter(i => i[1]).length);
+            setScore(currentScore);
             return newCartella;
         })
     }
 
-    return [cartella, handleNumberClick];
+    return [score, cartella, handleNumberClick];
 }
 
 export const Cartella = (props: CartellaProps) => {
     const { numbers, id } = props;
-    const [cartella, handleNumberClick] = useCartella([...numbers[0], ...numbers[1], ...numbers[2]]);
+    const [score, cartella, handleNumberClick] = useCartella([...numbers[0], ...numbers[1], ...numbers[2]]);
 
     return (
         <div>
-            <div className="title">Cartella no. {id}</div>
+            <div className="title">Cartella no. {id} - Score: {score}</div>
             <div className="cartella">
                 {numbers.map((row, idr) => row.map((number, idx) =>
                     number ?
--- src/pages/Cartelle.tsx
+++ src/pages/Cartelle.tsx
@@ -2,17 +2,30 @@ import { useParams } from "react-router";
 import { Cartella } from "../components/cartelle/Cartella"
 import { buildCartella } from "../components/utils/generaCartella";
 import './Cartelle.css';
+import { useMemo, useState } from "react";
 
 export const Cartelle = () => {
-    const { num } = useParams();
+    const initialScore = {
+        'Ambo': false,
+        'Terno': false,
+        'Quaterna': false,
+        'Cinquina': false,
+        'Tombola': false,
+    }
 
-    console.log("Cartelle render")
+    const { num } = useParams();
+    const [score, setScore] = useState(initialScore);
+
+    const buildCartelle = useMemo(() => {
+        console.log('Building Cartelle...');
+        return (num ? Array.from({ length: parseInt(num) }).map((_, i) => (
+            <Cartella key={i} numbers={buildCartella()} id={i + 1}></Cartella>
+        )) : <Cartella numbers={buildCartella()} id={1}></Cartella>)
+    }, [num])
 
     return (
         <div className='cartelle'>
-            {num ? Array.from({ length: parseInt(num) }).map((_, i) => (
-                <Cartella key={i} numbers={buildCartella()} id={i + 1}></Cartella>
-            )) : <Cartella numbers={buildCartella()} id={1}></Cartella>}
+            {buildCartelle};
         </div>
     )
 }
